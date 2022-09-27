5803343aba8bb71e99c5352dd2e2a13bd13b2f39
YoursMine-Server <sascha.u.kaufmann@googlemail.com>
2022-09-27 15:10:07 +0200

    style: prettier

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-
 class Watchdog {
   constructor(timeoutms, callback) {
-    this.timeoutms=timeoutms;
-    this.callback=callback;
+    this.timeoutms = timeoutms;
+    this.callback = callback;
     this.trigger();
   }
 
   trigger() {
-    if (typeof this.timer!="undefined") {
-      clearTimeout(this.timer)
+    if (typeof this.timer != 'undefined') {
+      clearTimeout(this.timer);
     }
-    this.timer=setTimeout(this.callback,this.timeoutms);
+    this.timer = setTimeout(this.callback, this.timeoutms);
   }
 }
 
--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-const WD_Class=require("./index.js");
-const WD = new WD_Class(10000,() => { console.log("DIE"); process.exit(); });
+const WD_Class = require('./index.js');
+const WD = new WD_Class(10000, () => {
+  console.log('DIE');
+  process.exit();
+});
 WD.trigger();
